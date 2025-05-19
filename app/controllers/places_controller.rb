2d993a890784359eacf9df4b43c16526a534172f
Stephen McCaffery <stephen.mccaffery@kellogg.northwestern.edu>
2025-05-19 19:29:18 +0000

    defined routes, created controllers, configured new index show

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,10 @@
+class PlacesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+resources "places"
+# get("/places", { :controller => "places", :action => "index" })
+# get("/places/new", { :controller => "places", :action => "new" })
+# post("/places", { :controller => "places", :action => "create" })
+# get("/places/:id", { :controller => "places", :action => "show" })
+
+resources "entries"
+# get("/places/:place_id/entries/new", { :controller => "entries", :action => "new" })
+# post("/places/:place_id/entries", { :controller => "entries", :action => "create" })
+
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
